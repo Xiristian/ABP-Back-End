411a7ceceae94ba783f4421bdac928d61142d793
christianhederson22@gmail.com <Xiristian>
2023-09-02 13:11:15 -0300

    Initial Commit

---
+++ src/main/java/org/example/model/EntitiyId.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+public class EntitiyId {
+    private Long id;
+
+    public EntitiyId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}

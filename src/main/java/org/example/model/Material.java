8ec6f31b24af22cd307d1a0d571532ff2c1a901e
judwluca <juliadcluca@hotmail.com>
2023-09-06 11:13:19 -0300

    Criação das classes: Material, ProdutoQuantitavel,ProdutoQuantitavelAtendimento e Remedio.

---
+++ src/main/java/org/example/model/Material.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+public class Material extends ProdutoQuantitavel{
+    public Material(Long id, String descricao, String observacao, Double valor) {
+        super(id, descricao, observacao, valor);
+    }
+}
---
+++ src/main/java/org/example/model/ProdutoQuantitavel.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+public class ProdutoQuantitavel extends Produto{
+    public ProdutoQuantitavel(Long id, String descricao, String observacao, Double valor) {
+        super(id, descricao, observacao, valor);
+    }
+}

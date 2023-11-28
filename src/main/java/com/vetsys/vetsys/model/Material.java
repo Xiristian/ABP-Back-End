6999994bea135761559c9340464197adfbeb09b0
Christian Giuliani <69261714+Xiristian@users.noreply.github.com>
2023-11-27 21:16:26 -0300

    Update Material.java

--- src/main/java/com/vetsys/vetsys/model/Material.java
+++ src/main/java/com/vetsys/vetsys/model/Material.java
@@ -5,34 +5,8 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("Material")
+@DiscriminatorValue("material")
 public class Material extends ProdutoQuantitavel{
-
-    @Column(name = "curativo")
-    private String curativo;
-
-    public String getCurativo() {
-        return curativo;
-    }
-
-    public void setCurativo(String curativo) {
-        this.curativo = curativo;
-    }
-
-    public String getFaixa() {
-        return faixa;
-    }
-
-    public void setFaixa(String faixa) {
-        this.faixa = faixa;
-    }
-
-    private String faixa;
-
-    public Material (String curativo ){
-        this.curativo = curativo;
-    }
-
     public Material() {
     }
 }
